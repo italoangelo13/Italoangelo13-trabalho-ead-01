4e38c836eba76fd5bcd352f6a80f4fddcead987a
Italo Angelo <italoangelo.ti@gmail.com>
2017-03-13 19:58:12 -0300

    corrigido

--- saida.php
+++ saida.php
@@ -95,7 +95,7 @@
 	        	<div class="container">
 	        		<div class="row">
 	        			<div class="col-lg-12">
-	        				<img src='<?php echo $imgBanda ?>' alt='<?php echo $nomeBanda; ?>' style="width: 100%;" >
+	        				<img src='<?php echo $imgBanda ?>' alt='<?php echo $nomeBanda; ?>' title='<?php echo $nomeBanda; ?>' style="width: 100%;" >
 	        			</div>
 	        		</div>
 
